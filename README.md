1a156cc14b8ce7671bd2ec321f86cc401212027c
drew-tom1 <tom.drew261@gmail.com>
2025-02-20 16:39:32 -0800

    preprocessing function created

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-### Instructions
+# Instructions
 
-# Dataset
+## Dataset
 
 Dataset came from Kaggle. I cut the number of entries to reduce loading time from ~35,000 down to 250. The link is pasted below:
 
 https://www.kaggle.com/datasets/jrobischon/wikipedia-movie-plots?resource=download
 
-# Setup
+## Setup
 
-The dependencies and versions are in the requirements.txt file.
+The dependencies and versions are in the requirements.txt file. Python version 3.12.9
 
 Please install the files with the command 
 
@@ -22,7 +22,7 @@ In macOS, the command is: ```source env/bin/activate```
 
 In Windows, the command is: ```env/Scripts/activate```
 
-# Run the program
+## Run the program
 
 Please run the program with the following command:
 
@@ -30,9 +30,11 @@ Please run the program with the following command:
 
 The program should prompt you for criteria and also `n` number of recommendations.
 
-# Example Output
+## Example Output
 
 Entry #, Title, Plot
 184  A Reckless Romeo  A philandering husband's public flirtation wit...
 74     A Film Johnnie  Charlie goes to the movies and falls in love w...
 91    The Masquerader  The Masquerader is a comedy short whose plot r...
+
+## Salary Expectation per Month
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import nltk
+import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('punkt_tab')
 
-# LOAD DATA THROUGH PANDAS (read_csv)
 def load_data():
+    """
+    Function to load and return the dataset
+
+    Parameters:
+    None
+
+    Returns:
+    file (csv): Kaggle Dataset of movie info.
+    """
     try:
         file = pd.read_csv("movie_info.csv")
         print("File loaded successfully")
@@ -12,12 +29,54 @@ def load_data():
         print("File did not load correctly. Please check file structure.")
     return file
 
-# COMPUTE RECOMMENDATIONS
+def text_preprocessing(text):
+    """
+    Preprocessing steps to reduce noise in the dataset entries.
+
+    - Converts characters to lowercase
+    - Removes punctuation
+    - Tokenizes words
+    - Removes stopwords
+    - Applies lemmatization.
+
+    Parameters:
+    text (str): Input text to be processed
+
+    Returns:
+    Clean and preprocessed text (str).
+
+    """
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer() 
+
+    text = str(text).lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    words = text.split()
+    words = word_tokenize(text)
+    words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
+
+    return ' '.join(words)
+
 def compute_similarity(query, dataset, top_n):
+    """
+    Function to compute similarity between user query and dataset entries.
+
+    Parameters:
+    query (str): User query of their criteria
+    dataset (pandas DataFrame): CSV file of movie information.
+    top_n (int): n number of recommendations wanted.
+
+    Returns:
+    recommendations (pandas DataFrame): n number of recommendations.
+    """
+    # APPLY PREPROCESSING TO DATA (USER INPUT AND DATASET)
+    dataset["Processed_Plot"] = dataset["Plot"].apply(text_preprocessing)
+    processed_query = text_preprocessing(query)
+
     # BUILD VECTORS
     vectorizer = TfidfVectorizer(stop_words="english")
-    tfidf_matrix = vectorizer.fit_transform(dataset["Plot"])
-    user_vector = vectorizer.transform([query])
+    tfidf_matrix = vectorizer.fit_transform(dataset["Processed_Plot"])
+    user_vector = vectorizer.transform([processed_query])
 
     # COMPUTE COSINE SIMILARITY AND ASSIGN RECOMMENDATIONS TO VARIABLE
     similiarity_score = cosine_similarity(user_vector, tfidf_matrix).flatten()
@@ -36,7 +95,7 @@ def main():
     try:
         num_matches = int(match_input)
         recs = compute_similarity(criteria_input, df, num_matches)
-        return recs
+        print(recs) # OUTPUT FINAL RECOMMENDATIONS
     except ValueError:
         print("ERROR: Invalid characters, please input an integer value for recommendations.")
     
